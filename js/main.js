92ebdaf0681428636cbb67f488041381f849a32a
PromtCooker <aleshakuleshov@gmail.com>
2025-11-30 16:06:10 +0300

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Базовый функционал для всего сайта
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Gonzo Cybersport website loaded');
+    
+    // Можно добавить общие функции здесь
+});
